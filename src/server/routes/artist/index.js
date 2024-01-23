c7e6ebbc630d9926981a4d2986ae03863f9d613b
trisrosco <trisrosco@gmail.com>
2024-01-23 12:32:39 +0000

    Swapped them back, added basic put but it doesn't work

--- src/server/routes/artist/index.js
+++ src/server/routes/artist/index.js
@@ -24,8 +24,16 @@ module.exports = function (router) {
   /**
    * Create a new artist
    */
-  router.post("/artists", async function (req, res) {
-    throw new Error("Not implemented");
+  router.put("/artists", async function (req, res) {
+    const artists = await Artist.findAll();
+    const newId = artists.length + 1; // At some point this should become a UUID, or something equally as robust.
+    const newArtist = await Artist.create({ id: newId, ...req.body });
+
+    if (!newArtist) {
+      return res.status(500).send("Error. Could not create new artist.");
+    }
+
+    res.status(201).json(newArtist);
   });
 
   /**
@@ -33,20 +41,22 @@ module.exports = function (router) {
    */
 
   // Not working yet. Will do once I've finished the rest of the app.
-  router.put("/artists/:id", async function (req, res) { 
+  router.post("/artists/:id", async function (req, res) {
     try {
-      const artistId = req.params.id; 
-      const updatedData = req.body; 
-      const artist = await Artist.findByPk(artistId); 
+      const artistId = req.params.id;
+      const updatedData = req.body;
+      const artist = await Artist.findByPk(artistId);
       if (!artist) {
         return res
           .status(404)
           .send("Could not find artist with ID: " + artistId);
       }
 
-      await artist.update(updatedData); 
-	  console.log(artist);
-      res.status(200).json({ message: 'Artist updated. ID: ', artistId, artist });
+      await artist.update(updatedData);
+      console.log(artist);
+      res
+        .status(200)
+        .json({ message: "Artist updated. ID: ", artistId, artist });
     } catch (error) {
       console.error(error);
       res.status(500).send("Error. Could not update ID: " + artistId);
