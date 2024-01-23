d42fa0db0494bb90fa45664efd4156238a206e2e
trisrosco <trisrosco@gmail.com>
2024-01-23 15:59:03 +0000

    Removed routes, going to remake them

--- src/server/routes/artist/index.js
+++ src/server/routes/artist/index.js
@@ -25,15 +25,7 @@ module.exports = function (router) {
    * Create a new artist
    */
   router.post("/artists", async function (req, res) {
-    const artists = await Artist.findAll();
-    const newId = artists.length + 1; // At some point this should become a UUID, or something equally as robust.
-    const newArtist = await Artist.create({ id: newId, ...req.body });
-
-    if (!newArtist) {
-      return res.status(500).send("Error. Could not create new artist.");
-    }
-
-    res.status(201).json(newArtist);
+    res.json({ message: "PLACEHOLDER CREATE" });
   });
 
   /**
@@ -42,25 +34,7 @@ module.exports = function (router) {
 
   // Not working yet. Will do once I've finished the rest of the app.
   router.put("/artists/:id", async function (req, res) {
-    try {
-      const artistId = req.params.id;
-      const updatedData = req.body;
-      const artist = await Artist.findByPk(artistId);
-      if (!artist) {
-        return res
-          .status(404)
-          .send("Could not find artist with ID: " + artistId);
-      }
-
-      await artist.update(updatedData);
-      console.log(artist);
-      res
-        .status(200)
-        .json({ message: "Artist updated. ID: ", artistId, artist });
-    } catch (error) {
-      console.error(error);
-      res.status(500).send("Error. Could not update ID: " + artistId);
-    }
+    res.json({ message: "PLACEHOLDER UPDATE" });
   });
 
   /**
@@ -68,10 +42,6 @@ module.exports = function (router) {
    */
   router.delete("/artists/:id", async function (req, res) {
     const artist = await Artist.findByPk(req.params.id);
-
-    if (!artist) {
-      return res.status(404).send("Could not find artist with ID: " + artistId);
-    }
 
     await artist.destroy();
 
