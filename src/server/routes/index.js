077a71bc73446f90d59f6832cca6152d5d630ed1
trisrosco <trisrosco@gmail.com>
2024-01-25 17:45:17 +0000

    Last minute whitespace removal

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -2,11 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 router.use(express.json()); // This parces the body of the request into JSON
-// It took a while to figure out why the body of the request was coming back as undefined, but I eventually found out that I needed to use this line of code to parse the body of the request into JSON. 
-
-
+// It took a while to figure out why the body of the request was coming back as undefined, but I eventually found out that I needed to use this line of code to parse the body of the request into JSON.
 
 require("./artist")(router);
 
 module.exports = router;
-
